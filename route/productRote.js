1d2d5dc8242e743fcc98c63c90875116763e9aec
YMendelzon <159305317+YMendelzon@users.noreply.github.com>
2024-07-14 20:33:56 +0300

    insert the project

--- route/productRote.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productController = require('../controllers/productController');
-
-router.get('/products', productController.getAllProducts);
-router.get('/products/:id', productController.getProductById);
-
-module.exports = router;
